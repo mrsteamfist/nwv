c432c03673572886f37feca6ba328f0c6524dedb
mrsteamfist_cp <SND\mrsteamfist_cp>
2015-08-27 02:57:57 +0000

    Added the example project to the solution

---
+++ Example/HadriansWall/HadriansWall.Windows/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Windows.Graphics.Display;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace HadriansWall
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private Rules rules = new Rules();
+        UserControl webControl;
+        public MainPage()
+        {
+            DisplayInformation.AutoRotationPreferences = DisplayOrientations.Landscape;
+            this.InitializeComponent();
+            rules.ReadyEvent += rules_ReadyEvent;
+            webControl = rules.WebControl;
+            webControl.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            root.Children.Add(webControl);
+        }
+        void rules_ReadyEvent(object sender, EventArgs e)
+        {
+            webControl.Visibility = Windows.UI.Xaml.Visibility.Visible;
+        }
+    }
+}
---
+++ Example/Rules/Resources.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Rules
+{
+    public static class Resources
+    {
+        public const string BACKGROUND_IMAGE = "~/Assets/board.jpg";
+    }
+}
